0bd3fe82dd2052edacc06964f3e11b8943f930b2
Vinicius Rodrigues da Silva <vinicius.rodrigues2004@gmail.com>
2024-11-21 22:59:49 -0300

    ajustes finais

--- app/src/main/java/br/com/vinighub/viniciusrodrigues_rm95828/MainActivity.kt
+++ app/src/main/java/br/com/vinighub/viniciusrodrigues_rm95828/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.lifecycle.ViewModelProvider
@@ -55,6 +56,7 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, GroupActivity::class.java)
             startActivity(intent)
         }
+
 
         val viewModelFactory = ItemsViewModelFactory(application)
 
